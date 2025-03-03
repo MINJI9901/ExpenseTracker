916cf8f05aede734eb756333377fbaf73d9cb45f
minji kim <rlaqufkr953@gmail.com>
2025-03-03 14:08:40 +0900

    eslint error in deployment trouble shooting

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -11,4 +11,5 @@ const compat = new FlatCompat({
 
 const eslintConfig = [...compat.extends("next/core-web-vitals")];
 
-export default eslintConfig;
+// export default eslintConfig;
+module.exports = eslintConfig;
